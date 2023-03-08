ec7ca9acdf832af8bff97f1af3242c3c00fdd046
FilippoFoti <filippofoti7@gmail.com>
2023-03-08 14:19:06 +0100

    Aggiunte le due domande

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Chiedere al cliente quanti chilometri vuole percorrere e quanti anni ha.
+alert('ATTENZIONE! \nQuesto è il sito ufficiale di Travel Whit Boolean. Qui potrai prenotare il tuo biglietto del treno. Buon Viaggio :)');
+
+// Chiedere al cliente quanti chilometri vuole percorrere trasformando la risposta in un numero.
+const kilometres = parseInt (prompt ("Benvenuto in Travel With Boolean, quanti chilometri vuoi percorrere?"));
+
+// Chiedere al cliente quanti anni ha trasformando la risposta in un numero
+const clientAge = parseInt (prompt ("In base alla tua età potrai avere uno sconto sul prezzo del biglietto. Quanti anni hai?"));
+console.log(kilometres, clientAge);
